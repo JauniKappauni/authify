e6e32d7acee9129c5a620b41fc4916825e7c35a0
JauniKappauni <social.jauni@gmail.com>
2025-03-23 14:02:32 +0100

    Added login route with post request handling and database check

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const env = require('dotenv')
 const app = express()
 const port = 3000
 const registerRouter = require('./routes/register')
+const loginRouter = require('./routes/login')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:true}));
@@ -11,5 +12,6 @@ app.get('/', (req,res) => {
     res.render('index')
 })
 app.use('/register', registerRouter);
+app.use('/login', loginRouter);
 
 app.listen(port, () => console.log(`Der Server läuft nun auf http://localhost:${port}`))
---
+++ routes/login.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const connection = require('../database')
+const router = express.Router()
+
+router.get('/', (req,res)=>{
+    res.render('login')
+})
+router.post('/', (req,res) => {
+    const {username, email, password} = req.body
+    const query = 'SELECT * FROM users WHERE username = ?'
+    connection.query(query, [username], (err, results) =>{
+        if(err){
+            console.error('Error:' + err)
+            res.send('Error' + err)
+        }
+        if(results.length == 0){
+            res.send('User not found')
+        }
+        const user = results[0]
+        if(user.password == password){
+            res.send('Sucessfully logged in')
+        }
+        else{
+            res.send('The password is wrong')
+        }
+    })
+})
+
+module.exports = router
