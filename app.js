3aad5bddb26f1e2992350199ea88086de8a1b3b2
JauniKappauni <social.jauni@gmail.com>
2025-03-23 15:15:34 +0100

    Added session handling Removed unnecessary Declarations and directly used them in the routes Added dashboard and logout route

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express')
+const session = require('express-session')
 const env = require('dotenv')
 const app = express()
 const port = 3000
-const registerRouter = require('./routes/register')
-const loginRouter = require('./routes/login')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:true}));
 
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
 app.get('/', (req,res) => {
     res.render('index')
 })
-app.use('/register', registerRouter);
-app.use('/login', loginRouter);
+app.use('/register', require('./routes/register'));
+app.use('/login', require('./routes/login'));
+app.use('/dashboard', require('./routes/dashboard'));
+app.use('/logout', require('./routes/logout'));
 
 app.listen(port, () => console.log(`Der Server läuft nun auf http://localhost:${port}`))
